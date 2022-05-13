fe728c4609022e6cb46b6388e6df0e7348e537b0
SushiMeliodas <bryanyf@hotmail.com>
2022-05-13 18:06:13 +0800

    Updated card list

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -4,7 +4,7 @@ import { useNavigation } from '@react-navigation/native';
 
 import { COLORS, SIZES, SHADOWS, assets } from '../constants';
 import { CircleButton, RectButton } from './Button';
-import SubInfo from './SubInfo';
+import { SubInfo, EthPrice, NFTTitle } from './SubInfo';
 
 const NFTCard = ({ data }) => {
   const navigation = useNavigation();
@@ -35,6 +35,31 @@ const NFTCard = ({ data }) => {
       <CircleButton imgUrl={assets.heart} right={10} top={18} />
 
       <SubInfo />
+
+      <View style={{ width: '100%', padding: SIZES.font }}>
+        <NFTTitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+        />
+
+        <View
+          style={{
+            marginTop: SIZES.font,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <EthPrice price={data.price} />
+          <RectButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate('Details', { data })}
+          />
+        </View>
+      </View>
     </View>
   );
 };
